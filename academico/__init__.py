1fcf0188cf269a6807fad87230cd92fd4f451c9e
Giancarlo Rocha <giancarloiff@gmail.com>
2018-11-20 20:01:15 -0200

    Refactored app to use serializers and models

--- academico/__init__.py
+++ academico/__init__.py
@@ -1,24 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import datetime
 import json
+import pdb
 
-from .parsers import Academico, DiarioParser
+from academico.parsers import DiarioParser
+from academico.database import db_session
+from academico.models import User, Diario, DiarioDescription
+from academico.serializers import UserSerializer, DiarioSerializer, DiarioDescriptionSerializer, ConfigSerializer
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 CORS(app)
 
-@app.route('/diario/<login>/<password>')
-def get_diario(login, password):
-    diario = DiarioParser(login=login, password=password)
-    res = diario.parse()
-    # return json.dumps(res, ensure_ascii=False)
-    return jsonify(res)
+@app.route('/diario/<matricula>/<senha>')
+def get_diario(matricula, senha):
+    user = UserSerializer(matricula, senha).get()
+    config = ConfigSerializer(user.id).get()
+
+    if config.need_to_update_diario():
+        parser = DiarioParser(matricula=matricula, senha=senha)
+        result = parser.parse()
+
+        for diario in result:
+            description = diario.pop('description', [])
+            tmp_diario = DiarioSerializer(user.id, **diario).get()
+
+            _ = [DiarioDescriptionSerializer(tmp_diario.id, **d).get() for d in description]
+
+    return jsonify(user.to_native())
 
 @app.route('/')
 def get_root():
     return jsonify({'info': 'Ok!'})
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
